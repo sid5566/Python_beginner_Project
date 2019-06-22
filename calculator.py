ab19acfcb81d014c453f26f4f91663e4a572164e
Siddharth Bathri <siddharthbathri@gmail.com>
2019-06-22 14:54:26 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,17 @@
+print('enter your value')
+aq=int(input())
+print('enter your operation')
+a=input(':')
+print('enter your value')
+aw=int(input())
+
+if a=='+':
+    print('your answer:',aq+aw)
+elif a== '-':
+    print('your answer:',aq-aw)
+elif a== '*':
+    print('your answer:',aq*aw)
+elif a== '/':
+    print('your answer:',aq/aw)
+else:
+    ('try again')
